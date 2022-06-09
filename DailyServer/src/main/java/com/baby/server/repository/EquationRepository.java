f939bf13907d3afa138a8d3e14684c1fe950ed28
Anderson <anderson.higo2@gmail.com>
2022-06-09 17:10:08 -0300

    build: init db Dockerfile

---
+++ DailyServer/src/main/java/com/baby/server/repository/EquationRepository.java
@@ -0,0 +1,9 @@
+package com.baby.server.repository;
+
+import com.baby.server.model.SecretEquations;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface EquationRepository extends JpaRepository<SecretEquations, Integer> {
+
+}
---
+++ Db/initdb.sql
@@ -0,0 +1,7 @@
+CREATE TABLE public.equation (    
+	ID SERIAL PRIMARY KEY,
+	equation varchar(7) NOT NULL
+);
+
+INSERT INTO public.equation ("10*4+2");
+
