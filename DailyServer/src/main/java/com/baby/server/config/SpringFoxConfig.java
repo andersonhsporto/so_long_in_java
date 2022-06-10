64944f73f280e4eedf21188348f88e43b8724458
Anderson <anderson.higo2@gmail.com>
2022-06-10 18:19:51 -0300

    build: otimize imports

--- DailyServer/src/main/java/com/baby/server/config/SpringFoxConfig.java
+++ DailyServer/src/main/java/com/baby/server/config/SpringFoxConfig.java
@@ -10,6 +10,7 @@ import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import java.util.ArrayList;
 
 @Configuration
--- DailyServer/src/main/java/com/baby/server/repository/EquationRepository.java
+++ DailyServer/src/main/java/com/baby/server/repository/EquationRepository.java
@@ -2,7 +2,6 @@ package com.baby.server.repository;
 
 import com.baby.server.model.SecretEquations;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface EquationRepository extends JpaRepository<SecretEquations, Integer> {
    SecretEquations findSecretEquationsById(int id);
