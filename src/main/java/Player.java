f8fd3324abcd4d55acbfbf4786ce9634e0bb2a42
Anderson <anderson.higo2@gmail.com>
2022-06-16 19:50:55 -0300

    build: working game

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,68 +1,105 @@
-import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
+import java.awt.Point;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Player {
+
+    // image that represents the player's position on the board
     private BufferedImage image;
-    private Point position;
-    private int counter;
+    // current position of the player on the board grid
+    private Point pos;
+    // keep track of the player's score
+    private int score;
 
     public Player() {
+        // load the assets
         loadImage();
-        position = new Point(0, 0);
-        counter = 0;
+
+        // initialize the state
+        pos = new Point(0, 0);
+        score = 0;
     }
 
-    public void loadImage() {
+    private void loadImage() {
         try {
-            image = ImageIO.read(getClass().getResourceAsStream("/player.png"));
-        } catch (Exception e) {
-            System.out.println("Error loading player image");
+            // you can use just the filename if the image file is in your
+            // project folder, otherwise you need to provide the file path.
+            image = ImageIO.read(new File("src/main/resources/images/player.png"));
+        } catch (IOException exc) {
+            System.out.println("Error opening image file: " + exc.getMessage());
         }
     }
 
-    public void render(Graphics g, ImageObserver observer) {
-        g.drawImage(image,
-                (position.x * Board.TILE_SIZE),
-                (position.y * Board.TILE_SIZE),
-                observer);
+    public void draw(Graphics g, ImageObserver observer) {
+        // with the Point class, note that pos.getX() returns a double, but
+        // pos.x reliably returns an int. https://stackoverflow.com/a/30220114/4655368
+        // this is also where we translate board grid position into a canvas pixel
+        // position by multiplying by the tile size.
+        g.drawImage(
+                image,
+                pos.x * Board.TILE_SIZE,
+                pos.y * Board.TILE_SIZE,
+                observer
+        );
     }
 
-    public void KeyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_W || e.getKeyCode() == KeyEvent.VK_UP) {
-            position.y--;
-        } else if (e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN) {
-            position.y++;
-        } else if (e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT) {
-            position.x--;
-        } else if (e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            position.x++;
+    public void keyPressed(KeyEvent e) {
+        // every keyboard get has a certain code. get the value of that code from the
+        // keyboard event so that we can compare it to KeyEvent constants
+        int key = e.getKeyCode();
+
+        // depending on which arrow key was pressed, we're going to move the player by
+        // one whole tile for this input
+        if (key == KeyEvent.VK_UP || key == KeyEvent.VK_W) {
+            pos.translate(0, -1);
+        }
+        if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D) {
+            pos.translate(1, 0);
+        }
+        if (key == KeyEvent.VK_DOWN || key == KeyEvent.VK_S) {
+            pos.translate(0, 1);
+        }
+        if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A) {
+            pos.translate(-1, 0);
+        }
+        if (key == KeyEvent.VK_ESCAPE) {
+            System.exit(0);
         }
     }
 
     public void tick() {
-        if (position.x < 0) {
-            position.x = 0;
-        } else if (position.x >= Board.COLUMNS - 1) {
-            position.x = Board.COLUMNS - 1;
-        } else if (position.y < 0) {
-            position.y = 0;
-        } else if (position.y >= Board.ROWS - 1) {
-            position.y = Board.ROWS - 1;
+        // this gets called once every tick, before the repainting process happens.
+        // so we can do anything needed in here to update the state of the player.
+
+        // prevent the player from moving off the edge of the board sideways
+        if (pos.x < 0) {
+            pos.x = 0;
+        } else if (pos.x >= Board.COLUMNS) {
+            pos.x = Board.COLUMNS - 1;
+        }
+        // prevent the player from moving off the edge of the board vertically
+        if (pos.y < 0) {
+            pos.y = 0;
+        } else if (pos.y >= Board.ROWS) {
+            pos.y = Board.ROWS - 1;
         }
     }
 
     public String getScore() {
-        return String.valueOf(counter);
+        return String.valueOf(score);
     }
 
-    public void addScore(int nbr) {
-        counter = counter + nbr;
+    public void addScore(int amount) {
+        score += amount;
     }
 
-    public Point getPosition() {
-        return position;
+    public Point getPos() {
+        return pos;
     }
+
 }
